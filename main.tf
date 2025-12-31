ea7e124f3c1fde7d5feda7fd671eb80ac9d42cc6
Skandhan Sivagnanam <skandhans@clouddestinations.com>
2025-12-31 13:03:24 +0530

    vpn

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 resource "azurerm_virtual_machine" "vm-1" {
     name                  = var.name 
-    location              = azurerm_resource_group.rg.location
-    resource_group_name   = azurerm_resource_group.rg.name
-    network_interface_ids = [azurerm_network_interface.nic-1.id]    
-}
+
+} 
